bc94a2bac32cc5fa0cd5a84b198b9b7d729882ee
iDepHub <idephub.scripts@gmail.com>
2026-08-15 02:30:03 -0400

    TestCenter

--- NytherNew.lua
+++ NytherNew.lua
@@ -386,7 +386,7 @@ contentLayout.Padding   = UDim.new(0, 12)
 contentLayout.Parent    = contentScroll
 
 local contentPad = Instance.new("UIPadding")
-contentPad.PaddingLeft   = UDim.new(0, 6)
+contentPad.PaddingLeft   = UDim.new(0, 16)
 contentPad.PaddingRight  = UDim.new(0, 16)
 contentPad.PaddingTop    = UDim.new(0, 16)
 contentPad.PaddingBottom = UDim.new(0, 16)
