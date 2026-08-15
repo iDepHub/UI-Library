02e6d187241f34a4d7a4985b5745692781926e5c
iDepHub <idephub.scripts@gmail.com>
2026-08-15 02:18:52 -0400

    NoLimitsV2

--- NytherNew.lua
+++ NytherNew.lua
@@ -1444,7 +1444,6 @@ local function NewColorPicker(parent, label, sub, defaultColor, callback, iconNa
         local c = getColor()
         preview.BackgroundColor3 = c
         square.BackgroundColor3 = Color3.fromHSV(H, 1, 1)
-        setAccentColor(c)
         if callback then callback(c) end
     end
 
